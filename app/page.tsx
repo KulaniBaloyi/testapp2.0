5ad45b2e86685f971f10732e94b36c941a4caa09
KulaniBaloyi <kulani17.kb@gmail.com>
2024-02-20 20:55:11 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-10 bg-cyan-950 text-white">Hey there <span className="text-3xl font-bold uppercase text-teal-300">Kulani</span> Welcome to your website</main>
+  );
+}
