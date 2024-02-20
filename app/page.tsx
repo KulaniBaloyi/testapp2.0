af1c90d0a407bfd1e049564dd7a88602727e2280
KulaniBaloyi <kulani17.kb@gmail.com>
2024-02-20 21:20:12 +0200

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="p-10 bg-cyan-950 text-white">Hey there <span className="text-3xl font-bold uppercase text-teal-300">Kulani</span> Welcome to your website</main>
+    <main className="p-10 bg-cyan-950 text-white">Hey there <span className=" uppercase text-teal-300">Kulani</span> Welcome to your website</main>
   );
 }
