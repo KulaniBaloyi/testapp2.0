80d5eb14eff645268eb88e1868053cdb67f6a713
KulaniBaloyi <kulani17.kb@gmail.com>
2024-02-20 23:46:27 +0200

    using latest version of Node

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="p-10 bg-cyan-950 text-white">Hey there <span className=" uppercase text-teal-300">Kulani</span> Welcome to your website</main>
+    <main className="h-screen grid place-content-center uppercase text-5xl font-bold bg-cyan-950 text-white">Hello World!!!</main>
   );
 }
